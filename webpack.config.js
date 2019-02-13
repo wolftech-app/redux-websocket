62d7493dd687a6ebd038c85cfb14dc8a25cd5686
Brian McAllister <brian@brianmcallister.com>
2019-02-13 11:57:27 -0500

    Configure webpack and babel to transform TypeScript.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+      }
+    ],
+  },
+};
