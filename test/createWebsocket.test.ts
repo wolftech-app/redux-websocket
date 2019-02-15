93543b5fdc75e0ebe8efd871f901c1de1bc75199
Brian McAllister <brian@brianmcallister.com>
2019-02-15 10:41:08 -0500

    Create createWebsocket specific tests.

---
+++ test/createWebsocket.test.ts
@@ -0,0 +1,92 @@
+import createWebsocket from '../src/createWebsocket';
+
+describe('createWebsocket', () => {
+  const dispatch = jest.fn(i => i).mockName('dispatch');
+  const ws = createWebsocket(dispatch, 'ws://fake.com');
+  const payload = { test: 'value' };
+
+  beforeEach(() => {
+    dispatch.mockClear();
+  });
+
+  describe('onopen', () => {
+    const expected = {
+      payload: {
+        event: payload,
+        timestamp: expect.any(Date),
+      },
+      type: 'WEBSOCKET:OPEN',
+    };
+
+    it('should return the dispatched action', () => {
+      if (ws.onopen) {
+        const result = ws.onopen(payload as any);
+
+        expect(result).toEqual(expected);
+      }
+    });
+
+    it('should dispatch the correct action.', () => {
+      if (ws.onopen) {
+        ws.onopen(payload as any);
+
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenCalledWith(expected);
+      }
+    });
+  });
+
+  describe('onclose', () => {
+    const expected = {
+      payload: {
+        event: payload,
+        timestamp: expect.any(Date),
+      },
+      type: 'WEBSOCKET:CLOSED',
+    };
+
+    it('should return the dispatched action', () => {
+      if (ws.onclose) {
+        const result = ws.onclose(payload as any);
+
+        expect(result).toEqual(expected);
+      }
+    });
+
+    it('should dispatch the correct action.', () => {
+      if (ws.onclose) {
+        ws.onclose(payload as any);
+
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenCalledWith(expected);
+      }
+    });
+  });
+
+  describe('onmessage', () => {
+    const expected = {
+      payload: {
+        event: payload,
+        timestamp: expect.any(Date),
+      },
+      type: 'WEBSOCKET:MESSAGE',
+    };
+
+    it('should return the dispatched action', () => {
+      if (ws.onmessage) {
+        const result = ws.onmessage(payload as any);
+
+        expect(result).toEqual(expected);
+      }
+    });
+
+    it('should dispatch the correct action.', () => {
+      if (ws.onmessage) {
+        ws.onmessage(payload as any);
+
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenCalledWith(expected);
+      }
+    });
+  });
+});
--- test/test.ts
+++ test/test.ts
@@ -11,62 +11,6 @@ jest.mock('../src/createWebsocket', () => {
 });
 
 describe('middleware', () => {
-  it('should dispatch the correct actions on initialization', () => {
-    // Mock everything out all the way down to the dispatch.
-    const wrapper = middleware({ getState: () => {}, dispatch: i => i });
-    const dispatch = wrapper(i => i);
-
-    // Dispatch a mock action.
-    dispatch({ type: 'WEBSOCKET:CONNECT', payload: { url: 'fake' } });
-
-    // Get the mocked websocket connection.
-    const ws = createWebsocket(i => i, 'fake url');
-
-    if (ws.onopen) {
-      const result = ws.onopen({ test: 'value' } as any);
-
-      expect(result).toEqual({
-        payload: {
-          event: {
-            test: 'value',
-          },
-          timestamp: expect.any(Date),
-        },
-        type: 'WEBSOCKET:OPEN',
-      });
-    }
-
-    if (ws.onclose) {
-      const result = ws.onclose({ test: 'value' } as any);
-
-      expect(result).toEqual({
-        payload: {
-          event: {
-            test: 'value',
-          },
-          timestamp: expect.any(Date),
-        },
-        type: 'WEBSOCKET:CLOSED',
-      });
-    }
-
-    if (ws.onmessage) {
-      const result = ws.onmessage({ test: 'value', data: 'test' } as any);
-
-      expect(result).toEqual({
-        payload: {
-          data: 'test',
-          event: {
-            data: 'test',
-            test: 'value',
-          },
-          timestamp: expect.any(Date),
-        },
-        type: 'WEBSOCKET:MESSAGE',
-      });
-    }
-  });
-
   it('should close correctly', () => {
     // Mock everything out all the way down to the dispatch.
     const wrapper = middleware({ getState: () => {}, dispatch: i => i });
