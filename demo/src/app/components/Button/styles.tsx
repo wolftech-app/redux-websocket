8d24b8fc9d95c9f38d953671e2685839f61b09fc
Brian McAllister <brian@brianmcallister.com>
2019-04-02 09:29:25 -0400

    Tweak button styling.

--- demo/src/app/components/Button/styles.tsx
+++ demo/src/app/components/Button/styles.tsx
@@ -6,15 +6,16 @@ import Colors from '../../styles/js/colors';
 
 export default styled.button`
   ${Typography.TypeStyleArcturus}
-
   background-color: ${Colors.MARS};
+  border-radius: 2px;
   border: none;
-  border-radius: 3px;
   color: ${Colors.SUN};
-  display: block;
+  display: inline-block;
   outline: none;
-  padding: 5px 15px;
+  padding: 7px 15px 6px 15px;
   text-transform: uppercase;
+  transition-property: background-color, color;
+  transition: 0.1s ease-in-out;
 
   &:hover {
     background-color: ${lighten(0.02, Colors.MARS)};
