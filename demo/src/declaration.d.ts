c5f2d81375a31fea13bb79d989b827213354939c
Brian McAllister <brian@brianmcallister.com>
2019-04-17 17:39:35 -0400

    Track how many messages are sent with StatHat.

--- demo/src/declaration.d.ts
+++ demo/src/declaration.d.ts
@@ -1,5 +1,12 @@
 declare const process : {
   env: {
     REDUX_WEBSOCKET_VERSION: string
+    NODE_ENV: 'development' | 'production'
   }
 };
+
+interface Window {
+  _StatHat: {
+    push: (args: [string, string, number]) => void;
+  }
+}
