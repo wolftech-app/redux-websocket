282a11ad98ff24e68ed2b73b8e36a514b8cac76d
Brian McAllister <brian@brianmcallister.com>
2019-04-25 14:01:55 -0400

    Move actions test file.

--- src/__tests__/actions.test.ts
+++ src/__tests__/actions.test.ts
@@ -1,5 +1,5 @@
-import * as actions from '../src/actions';
-import * as actionTypes from '../src/actionTypes';
+import * as actions from '../actions';
+import * as actionTypes from '../actionTypes';
 
 describe('actions', () => {
   describe('closed', () => {
