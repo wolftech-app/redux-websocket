a7cfa86c9e363e6c54c8677db0143a182bfca56f
Alfonso Millan <alfonso.millan@giantmachines.com>
2020-04-24 13:37:29 -0400

    feat: extend eslint-config-prettier in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'plugin:eslint-comments/recommended',
     'plugin:jest/recommended',
     'plugin:promise/recommended',
+    'eslint-config-prettier',
   ],
   rules: {
     'indent': ['error', 2, {
