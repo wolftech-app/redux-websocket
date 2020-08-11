2d00b5cb20889d48e3d164bc84e1aae005576006
Ho Won  Cheng <howon.cheng@giantmachines.com>
2020-08-11 14:54:30 -0400

    Add more thoughts and maybe hooks

--- src/types.d.ts
+++ src/types.d.ts
@@ -49,31 +49,72 @@ declare function createMiddleware(options?: Options): Middleware;
  *
  */
 
-createMiddleware;
-new ReduxWebSocketsManager();
-Whenever connect, disconnect, send are dispatched send to ReduxWebSocketsManager
-ReduxWebSocketsManager onConnect ReduxWebSocketsManager.connection
+// createMiddleware;
+// new ReduxWebSocketsManager();
+// Whenever connect, disconnect, send are dispatched send to ReduxWebSocketsManager
+// ReduxWebSocketsManager onConnect ReduxWebSocketsManager.connection
 
 class ReduxWebSocketsManager {
   // Class options.
   private connections: { [url: string]: ReduxWebSocket };
+
   constructor(options) {
     this.options = options;
     this.connections = {};
   }
 
-  onConnect(connectAction: { payload: {id: string} }) {
-    const connection = this.connections[connectAction.payload.url] || new ReduxWebSocket(this.options);
+  onConnect(connectAction: { payload: { id: string } }) {
+    const connection =
+      this.connections[connectAction.payload.url] ||
+      new ReduxWebSocket(this.options);
     connection.connect(connectAction);
   }
 
   onDisconnect(disconnectAction) {
-    const connection = this.connections[connectAction.payload.url] || new ReduxWebSocket(this.options);
-    connection.connect(connectAction);
+    // if there is a prefix, dispatch to that one, else dispatch to all
+    const connection = this.connections[connectAction.payload.url];
+    connection.disconnect(connectAction);
   }
 
   onSend(sendAction) {
-    const connection = this.connections[connectAction.payload.url] || new ReduxWebSocket(this.options);
-    connection.connect(connectAction);
+    // if there is a prefix, dispatch to that one, else dispatch to all
+    const connection = this.connections[connectAction.payload.url];
+    connection.send(connectAction);
   }
 }
+
+// dispatch(connect('ws://something'));
+// dispatch(connect('ws://something2'));
+
+// type Status = 'CONNECTING' | 'OPEN' | 'CLOSING' | 'CLOSED' | null;
+
+// redux - websocket;
+// useReduxWebsocket;
+
+// const dispatch = useDispatch();
+// const {
+//   connect,
+//   disconnect,
+//   send,
+//   getReadyState,
+//   getWebsocket,
+// } = useReduxWebsocket('url');
+// dispatch(actions.connect());
+// dispatch(actions.send('message'));
+
+// const useReduxWebsocket = (config: Config | string) => {
+//   const dispatch = getDispatch();
+//   dispatch(connect(config));
+//   // This would somehow get an instance of the manager
+//   // create or find the connection
+//   const connection = 'Something';
+//   return {
+//     connect: () => connect(url),
+//     disconnect: () => disconnect(url),
+//     send: () => send(url),
+//     getReadyState: () => connection.websocket?.readyState || null,
+//     getWebsocket: () => connection.websocket,
+//   };
+// };
+
+// const { actions, readyState, websocket } = useReduxWebsocket('url');
