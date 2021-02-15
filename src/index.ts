886a5cb93b2ffd759f8117cac51bab07e6a5c2c2
Sergey Kucheyko <dr.grey_101by@inbox.ru>
2021-02-15 17:18:50 +0300

    fixed in accordance with comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { connect, send, disconnect, broken, beginReconnect, reconnectAttempt } from './actions';
+import { connect, send, disconnect } from './actions';
 import createMiddleware from './createMiddleware';
 
 export * from './actionTypes';
 
-export { connect, createMiddleware as default, disconnect, send, broken, beginReconnect, reconnectAttempt };
+export { connect, createMiddleware as default, disconnect, send };
